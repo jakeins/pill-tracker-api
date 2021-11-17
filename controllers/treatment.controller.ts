e7e5a64aac3b7c40fd847e3291003851016528a1
Max Nazarenko <maksyrup@gmail.com>
2021-11-17 17:58:10 +0200

    Add guids to treatment children

--- controllers/treatment.controller.ts
+++ controllers/treatment.controller.ts
@@ -1,5 +1,6 @@
 import { DELETE, GET, Path, PathParam, POST, PUT } from 'typescript-rest';
 import { Tags } from 'typescript-rest-swagger';
+import { v4 } from 'uuid';
 
 import { ServiceContainer } from '../containers';
 import { ITreatment } from '../models';
@@ -12,6 +13,17 @@ export class TreatmentController {
   @Path('/treatment')
   @POST
   public create(input: ITreatment): ITreatment {
+
+    // Auto-generate GUIDs
+    input.medications.forEach(medication => {
+      medication.guid = v4();
+      medication.schedule.guid = v4();
+
+      medication.schedule.takes.forEach(take => {
+        take.guid = v4();
+      });
+    });
+
     const result = this.treatmentRepo.Create(input) as ITreatment;
     return result;
   }
